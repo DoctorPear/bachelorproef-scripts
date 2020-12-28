1d1314997fcf6a4e13216cbdcb7c3f2201699c1f
DoctorPear <robinpeerlinck@gmail.com>
2020-12-28 13:50:55 +0100

    Add files via upload

--- kafkaBenchmark.py
+++ kafkaBenchmark.py
@@ -5,7 +5,7 @@ import threading, logging, time, json, sys, os
 from kafka import KafkaConsumer, KafkaProducer
 
 msg_size = 1000
-amount_of_messages = sys.argv[2]
+amount_of_messages = 10
 
 producer_stop = threading.Event()
 consumer_stop = threading.Event()
