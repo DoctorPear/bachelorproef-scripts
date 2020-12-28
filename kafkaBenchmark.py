3fddc50931b33d527abca89eca055a4256767ace
DoctorPear <robinpeerlinck@gmail.com>
2020-12-28 13:15:16 +0100

    Add files via upload

--- kafkaBenchmark.py
+++ kafkaBenchmark.py
@@ -36,7 +36,7 @@ class Consumer(threading.Thread):
         for message in consumer:
             result = message
             print(result)
-            adobj = {"sendtime": result[0], "receivetime": int(round(time.time() * 1000))}
+            adobj = {"sendtime": result[6]['sendtime'], "receivetime": int(round(time.time() * 1000))}
             json_object_file["records"].append(adobj)
 
             if consumer_stop.is_set():
